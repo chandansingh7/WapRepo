797f8a6ef88cc24cffd6c09235ef50c7e70e11d1
chandansingh7 <chandan.singh558@gmail.com>
2021-07-27 01:29:26 -0500

    Update regexscript.js

--- forms-testing/src/regexscript.js
+++ forms-testing/src/regexscript.js
@@ -1,8 +1,6 @@
 window.onload = function() {
     let submitButton = document.getElementById("submit");
-    submitButton.disabled = true;
-
-    
+    submitButton.disabled = true;    
     document.getElementById("exampleInputPassword1").onkeyup = function() {
         let regex =  /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,}$/;
         if(this.value.match(regex)) {
