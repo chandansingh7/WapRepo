852192f4269b630aeec510971e0b1b76d8673900
chandansingh7 <chandan.singh558@gmail.com>
2021-07-27 11:10:56 -0500

    Add Product Button Functions

--- forms-testing/src/tests.js
+++ forms-testing/src/tests.js
@@ -1,13 +1,17 @@
 window.onload = function() {
     let dateValidate = document.getElementById("dateError");
+    let addProduct = document.getElementById("addProduct");
     dateValidate.style.display = "none";
+    addProduct.disabled = true;
 
     document.getElementById("dateSupplied").onkeyup = function() {
         let datereg = /^\d{4}\-(0[1-9]|1[012])\-(0[1-9]|[12][0-9]|3[01])$/;
         if(this.value.match(datereg)){
-            dateValidate.style.display = "none";           
+            dateValidate.style.display = "none"; 
+            addProduct.disabled = false;          
         }else{
             dateValidate.style.display = "block"; 
+            addProduct.disabled = true;
         }
     }
 }
