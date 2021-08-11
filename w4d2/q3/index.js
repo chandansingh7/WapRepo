4ad3898e1c890b0087806e21e27269a62466ac32
chandansingh7 <chandan.singh558@gmail.com>
2021-08-11 02:33:12 -0500

    w4d2 complete assignment

---
+++ w4d2/q3/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const router  = require("./router");
+const path    = require('path');
+const session = require('express-session');
+const app     = express();
+
+app.use(session({
+    secret: '2AF02B3C406C7650000C18CD2C590C8E5B47E2BB9D8A4EFDEDB6B71439A7E60BC09C7D3C82C8C9952DE214B2AF1EC5042D25275DE1C0199057993F00642AB1CE',
+    resave: false,
+    saveUninitialized: true
+}));
+
+app.set('view engine', 'ejs');
+
+app.set('views', path.join(__dirname, "views"));
+app.use('/js', express.static(path.join(__dirname, 'views', 'js')));
+
+app.use(express.urlencoded({extended:false}));
+
+app.use('/products', router);
+
+app.listen(3000);
